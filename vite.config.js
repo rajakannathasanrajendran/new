aaf9caab3a55cdf9361cf36d4be724872c7116b2
rajakannathasanmaghil <rajakannathasan@gmail.com>
2026-04-01 12:30:57 +0530

    Com
    Com

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',              // ✅ important for domain root
   plugins: [react()],
+
+  build: {
+    outDir: 'dist'        // ✅ build output folder
+  }
 })
